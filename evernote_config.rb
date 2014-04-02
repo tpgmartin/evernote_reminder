b22ec2da22fec84854c8f217799b68330f11ffbe
Thomas Martin <tpgmartin@gmail.com>
2014-04-02 06:22:20 +0100

    Initial commit, following Evernote SDK

---
+++ evernote_config.rb
@@ -0,0 +1,13 @@
+# Load libraries required by the Evernote OAuth
+require 'oauth'
+require 'oauth/consumer'
+ 
+# Load Thrift & Evernote Ruby libraries
+require "evernote_oauth"
+ 
+# Client credentials
+OAUTH_CONSUMER_KEY = "tpgmartin"
+OAUTH_CONSUMER_SECRET = "241f74a8650ce1f5"
+ 
+# Connect to Sandbox server?
+SANDBOX = true
